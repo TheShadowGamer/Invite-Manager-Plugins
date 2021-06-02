a72c9145938b9ae2e5c504ff25d430205b157611
itzJOHv <68508885+itzJOHv@users.noreply.github.com>
2021-06-02 18:12:01 +0300

    Fix Typo

--- giveaway-plugin/src/functions/endGiveaway.js
+++ giveaway-plugin/src/functions/endGiveaway.js
@@ -28,16 +28,16 @@ module.exports = async (client, entry) => {
     let embed = new MessageEmbed(message.embeds[0]);
     let member = reaction.users.cache.filter(user => !user.bot);
     member = member.randomKey(entry.winners);
-    if(members) await message.channel.send(`:tada: Congratulations ${member.map(member => `<@${member}>`).join(' ') || `<@${member}>`}, you won **${embed.title}** <${message.url}>`);
-    if(entry.winners === 1 && members) {
+    if(member) await message.channel.send(`:tada: Congratulations ${member.map(member => `<@${member}>`).join(' ') || `<@${member}>`}, you won **${embed.title}** <${message.url}>`);
+    if(entry.winners === 1 && member) {
         embed.setDescription(`Giveaway has ended!\n\n**Winner**: ${member.map(member => `<@${member}>`).join(' ') || `<@${member}>`}\nHost: <@${entry.host}>`);
     } else {
         embed.setDescription(`Giveaway has ended!\n\n**Winners**: ${member.map(member => `<@${member}>`).join(' ') || `<@${member}>`}\nHost: <@${entry.host}>`);
     };
     embed.setColor('RED')
     .setFooter(`${embed.footer.text.split('|')[0]} | Ended at`);
-    if(members) await message.edit(embed);
-    if(!members) message.channel.send(`The **${embed.title}** <${message.url}> giveaway has ended but no one won!`);
+    if(member) await message.edit(embed);
+    if(!member) message.channel.send(`The **${embed.title}** <${message.url}> giveaway has ended but no one won!`);
     entry.update({
         deleteAt: new Date(Date.now() + 172800000),
     }, {
